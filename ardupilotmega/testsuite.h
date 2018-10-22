e211945ba436e7fc8c0cf7a22c3ce3b3f09130df
Uavosky <kapacheuski@gmail.com>
2018-10-22 16:22:47 +0300

    MAVLINK with landing_target_status updated

--- ardupilotmega/testsuite.h
+++ ardupilotmega/testsuite.h
@@ -3583,6 +3583,67 @@ static void mavlink_test_esc_telemetry_9_to_12(uint8_t system_id, uint8_t compon
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 }
 
+static void mavlink_test_landing_target_status(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+    mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_LANDING_TARGET_STATUS >= 256) {
+            return;
+        }
+#endif
+    mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+    mavlink_landing_target_status_t packet_in = {
+        93372036854775807ULL,73.0,101.0,129.0,65,132,199,10
+    };
+    mavlink_landing_target_status_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        packet1.time_usec = packet_in.time_usec;
+        packet1.offset_x = packet_in.offset_x;
+        packet1.offset_y = packet_in.offset_y;
+        packet1.distance = packet_in.distance;
+        packet1.target_num = packet_in.target_num;
+        packet1.frame = packet_in.frame;
+        packet1.type = packet_in.type;
+        packet1.trust_level = packet_in.trust_level;
+        
+        
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+        if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
+           // cope with extensions
+           memset(MAVLINK_MSG_ID_LANDING_TARGET_STATUS_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_LANDING_TARGET_STATUS_MIN_LEN);
+        }
+#endif
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_landing_target_status_encode(system_id, component_id, &msg, &packet1);
+    mavlink_msg_landing_target_status_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_landing_target_status_pack(system_id, component_id, &msg , packet1.time_usec , packet1.target_num , packet1.frame , packet1.offset_x , packet1.offset_y , packet1.distance , packet1.type , packet1.trust_level );
+    mavlink_msg_landing_target_status_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_landing_target_status_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.time_usec , packet1.target_num , packet1.frame , packet1.offset_x , packet1.offset_y , packet1.distance , packet1.type , packet1.trust_level );
+    mavlink_msg_landing_target_status_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+        mavlink_msg_to_send_buffer(buffer, &msg);
+        for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
+            comm_send_ch(MAVLINK_COMM_0, buffer[i]);
+        }
+    mavlink_msg_landing_target_status_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_landing_target_status_send(MAVLINK_COMM_1 , packet1.time_usec , packet1.target_num , packet1.frame , packet1.offset_x , packet1.offset_y , packet1.distance , packet1.type , packet1.trust_level );
+    mavlink_msg_landing_target_status_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+}
+
 static void mavlink_test_ardupilotmega(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
     mavlink_test_sensor_offsets(system_id, component_id, last_msg);
@@ -3645,6 +3706,7 @@ static void mavlink_test_ardupilotmega(uint8_t system_id, uint8_t component_id, 
     mavlink_test_esc_telemetry_1_to_4(system_id, component_id, last_msg);
     mavlink_test_esc_telemetry_5_to_8(system_id, component_id, last_msg);
     mavlink_test_esc_telemetry_9_to_12(system_id, component_id, last_msg);
+    mavlink_test_landing_target_status(system_id, component_id, last_msg);
 }
 
 #ifdef __cplusplus
